e8fcd55df1681d50246fced4030a2232b7242cac
Anurag Mahajan <anurag.mahajan@viitor.cloud>
2026-05-22 12:36:06 +0530

    Change's for the frontpage.java

--- mathapplicationv2/src/test/java/mathapplication/frontpage.java
+++ mathapplicationv2/src/test/java/mathapplication/frontpage.java
@@ -1,8 +1,17 @@
 package mathapplication;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
 public class frontpage {
     public static void main(String[] args) {
         System.out.println("Test");
+        System.out.println("Test");
+        System.setProperty("webdriver.chrome.driver", "path/to/chromedriver");
+        WebDriver driver = new ChromeDriver();
+        driver.get("https://uat-new-school-cms.content.classrootsedu.com/auth/login");
+        driver.manage().window().maximize();
+        driver.quit();
     }
 
 }
